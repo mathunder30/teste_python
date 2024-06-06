6e99e1d36be2092cd054fa5bcd72011bc0091188
Mateus Lopes <mateustlopes11@gmail.com>
2024-06-06 18:03:41 -0300

    mensagem

---
+++ contagem.py
@@ -0,0 +1,6 @@
+num1 = int(input("digite um numero: "))
+num2 = int(input("Digite um numero: "))
+for x in range(num1, num2, -1):
+ print(x)
+print("feliz ano novo")
+
---
+++ somar.py
@@ -0,0 +1,5 @@
+num1 = int(input("digite um numero: "))
+soma = 0
+for x in range (0,num1 + 1):
+    soma = soma + x
+print(soma)
